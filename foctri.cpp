35be1fb6c7d6a61e433481f6e744fe420010b798
Steve Wang <74487788+SteveSWang47@users.noreply.github.com>
2021-10-16 15:42:29 +0800

    Add files via upload

---
+++ foctri.cpp
@@ -0,0 +1,35 @@
+#include <ncurses.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+
+using namespace std;
+
+int c[9], pla;
+
+int main(int argc, char **argv)
+{
+    initscr();
+    curs_set(0);
+    raw();
+    keypad(stdscr, TRUE);
+    while(1)
+    {
+        memset(c, 0, sizeof(c));
+        srand(time(NULL));
+        for(int i = 1; i <= 9; i++)
+        {
+            do{pla = rand() % 9;}while(c[pla]);
+            c[pla] = i;
+        }
+        clear();
+        mvprintw(LINES/2-2, COLS/2-2, "%d %d %d", c[0], c[1], c[2]);
+        mvprintw(LINES/2, COLS/2-2, "%d %d %d", c[3], c[4], c[5]);
+        mvprintw(LINES/2+2, COLS/2-2, "%d %d %d", c[6], c[7], c[8]);
+        refresh();
+        char t = getch();
+        if(t == 'q') break;
+    }
+    endwin();
+    return 0;
+}
